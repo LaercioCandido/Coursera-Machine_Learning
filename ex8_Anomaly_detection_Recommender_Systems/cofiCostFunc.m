b87cda0628e95df5ac34666fe597e94ea4b15153
laerciocandido <lcs_junior@yahoo.com.br>
2021-02-27 23:09:16 -0300

    add ex8

--- ex8_Anomaly_detection_Recommender_Systems/cofiCostFunc.m
+++ ex8_Anomaly_detection_Recommender_Systems/cofiCostFunc.m
@@ -40,16 +40,21 @@ Theta_grad = zeros(size(Theta));
 %                     partial derivatives w.r.t. to each element of Theta
 %
 
+%Função custo
+diff = (X*Theta' - Y) .* R;
+J = sum(sum(diff.^2))/2;
 
+% Regularizando J
+J = J + lambda*sum(sum(Theta.^2))/2;
+J = J + lambda*sum(sum(X.^2))/2;    
 
+%Implementação não regularizada
+X_grad = (diff.*R)*Theta;                 
+Theta_grad = ((diff.*R)'*X);             
 
-
-
-
-
-
-
-
+%Implementação regularizada
+X_grad = X_grad + (lambda * X);         
+Theta_grad = Theta_grad + (lambda * Theta);
 
 
 
